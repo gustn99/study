07924eae393c3615f30cf2b0b3fab80b967ca4fe
young <azerosong@gmail.com>
2024-01-28 01:12:41 +0900

    24/01/28 생활코딩 WEB2 34: 분할구현

---
+++ 생활코딩/colors.js
@@ -0,0 +1,31 @@
+var Links = {
+  setColor:function(color) {
+      var aList = document.querySelectorAll('a');
+      var i = 0;
+      while(i < aList.length) {
+          aList[i].style.color = color;
+          i = i + 1;
+      }
+  }
+}
+var Body = {
+  setColor:function(color) {
+      document.querySelector('body').style.color = color;
+  },
+  setBackgroundColor:function(color) {
+      document.querySelector('body').style.backgroundColor = color;
+  }
+}
+function nightDayHandler(self) {
+  if(self.value === 'night') {
+      Body.setBackgroundColor('black');
+      Body.setColor('white');
+      self.value = 'day';
+      Links.setColor('pink');
+  } else {
+      Body.setBackgroundColor('white');
+      Body.setColor('black');
+      self.value = 'night';
+      Links.setColor('green')
+  }
+}
