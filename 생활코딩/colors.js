d45bb87f10e4073f847eab265acbe50a46c0eae5
young <azerosong@gmail.com>
2024-01-28 01:40:13 +0900

    24/01/28 생활코딩 WEB2 35: library framework

--- 생활코딩/colors.js
+++ 생활코딩/colors.js
@@ -1,19 +1,22 @@
 var Links = {
   setColor:function(color) {
-      var aList = document.querySelectorAll('a');
-      var i = 0;
-      while(i < aList.length) {
-          aList[i].style.color = color;
-          i = i + 1;
-      }
+      // var aList = document.querySelectorAll('a');
+      // var i = 0;
+      // while(i < aList.length) {
+      //     aList[i].style.color = color;
+      //     i = i + 1;
+      // }
+      $('a').css('color', color);
   }
 }
 var Body = {
   setColor:function(color) {
-      document.querySelector('body').style.color = color;
+      // document.querySelector('body').style.color = color;
+      $('body').css('color', color);
   },
   setBackgroundColor:function(color) {
-      document.querySelector('body').style.backgroundColor = color;
+      // document.querySelector('body').style.backgroundColor = color;
+      $('body').css('backgroundColor', color);
   }
 }
 function nightDayHandler(self) {
